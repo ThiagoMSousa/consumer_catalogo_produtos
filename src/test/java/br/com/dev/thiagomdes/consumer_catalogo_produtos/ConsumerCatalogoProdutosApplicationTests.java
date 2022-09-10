9a0341d21e2b7d9e8c59be90d3c05d5525bdbc3e
Thiago Mendes de Sousa <thiagomendes_95@hotmail.com>
2022-09-09 22:20:35 -0300

    Criado Arquivo Dockerfile

--- src/test/java/br/com/dev/thiagomdes/consumer_catalogo_produtos/ConsumerCatalogoProdutosApplicationTests.java
+++ src/test/java/br/com/dev/thiagomdes/consumer_catalogo_produtos/ConsumerCatalogoProdutosApplicationTests.java
@@ -1,4 +1,4 @@
-package thiagomdes.consumer_catalogo_produtos;
+package br.com.dev.thiagomdes.consumer_catalogo_produtos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
