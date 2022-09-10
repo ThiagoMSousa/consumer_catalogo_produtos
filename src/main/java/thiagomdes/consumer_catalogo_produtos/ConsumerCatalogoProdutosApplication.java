8bb935904a835a10f2cf405a24398bf1914a450e
Thiago Mendes de Sousa <thiagomendes_95@hotmail.com>
2022-09-09 22:00:04 -0300

    Configuração Inicial Aplicação

---
+++ src/main/java/thiagomdes/consumer_catalogo_produtos/ConsumerCatalogoProdutosApplication.java
@@ -0,0 +1,13 @@
+package thiagomdes.consumer_catalogo_produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerCatalogoProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerCatalogoProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thiagomdes/consumer_catalogo_produtos/ConsumerCatalogoProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package thiagomdes.consumer_catalogo_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerCatalogoProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
