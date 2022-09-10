b99ce481b4d2d8aaff62a1134120ff568f2258dc
Thiago Mendes de Sousa <thiagomendes_95@hotmail.com>
2022-09-10 11:00:57 -0300

    Ajustado ClassMain no Dockerfile e Package Name

--- src/main/java/br/com/dev/thiagomds/consumer_catalogo_produtos/ConsumerCatalogoProdutosApplication.java
+++ src/main/java/br/com/dev/thiagomds/consumer_catalogo_produtos/ConsumerCatalogoProdutosApplication.java
@@ -1,4 +1,4 @@
-package br.com.dev.thiagomdes.consumer_catalogo_produtos;
+package br.com.dev.thiagomds.consumer_catalogo_produtos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/dev/thiagomds/consumer_catalogo_produtos/ConsumerCatalogoProdutosApplicationTests.java
+++ src/test/java/br/com/dev/thiagomds/consumer_catalogo_produtos/ConsumerCatalogoProdutosApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.dev.thiagomdes.consumer_catalogo_produtos;
+package br.com.dev.thiagomds.consumer_catalogo_produtos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
